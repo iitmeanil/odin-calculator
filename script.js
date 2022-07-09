39653fc7bdd47e0f485708fa3615b057aaf346cc
Anil Kumar Raju <iitmeanil@gmail.com>
2022-07-09 19:38:45 +0530

    breaks the input into three buckets for diff actions

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 // and displaying it on screen. 
 
 
+
+let screenArr = []; // what gets displayed on the screen 
+let num; // number that is getting entered with every click
+let processingArr = []; // this will have all the elements for processing when one presses ""="
+
 const buttons = document.querySelectorAll(".button");
 const screen = document.querySelector(".screen");
-let inputArr = [];
+
 buttons.forEach((button) => {
 button.addEventListener ('click', processClick)
 });
@@ -12,10 +17,36 @@ button.addEventListener ('click', processClick)
 
 // three things can happen - 
 //1. presses numbers or ., then all gets concatenated. 
-// 2. operators, then previous numbers gets added to processing array and operator gets a
-
+//2. operators, then previous numbers gets added to processing array and operator gets added to processing array
+//3. presses = then clears the screen and we need to process the processing string 
 function processClick(e) {
-    console.log(e.target.id);
+    //console.log(e.target.id);
+    //console.log(typeof(e.target.id));
+
+    switch (e.target.id) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case ".":
+            console.log("Number");
+            break;
+        case "*":
+        case "/":
+        case "+":
+        case "-":
+            console.log("Operator");
+            break;
+        case "=":
+            console.log("equal");             
+    
+    }
 
 }
 
